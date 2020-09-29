0f621710fa70a16b329a55d3778ec3b28822ed06
bill-rich <bill.rich@gmail.com>
2020-09-29 09:18:50 -0700

    fixed pulling

--- role.runner.js
+++ role.runner.js
@@ -17,21 +17,24 @@ module.exports = class {
       creep.memory.target = ""
     }
     if(!creep.memory.target){
-      creep.pos.findClosestByPath(FIND_MY_CREEPS, {
+      let target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
         filter: (towCreep) => {
           return (!util.isTargeted(towCreep) &&
-                  creep.memory.dest
+                  towCreep.memory.dest
           )
         }
       })
+      if(target){
+        creep.memory.target = target.id
+      }
     }
     if(creep.memory.target){
       let target = Game.getObjectById(creep.memory.target)
-			if(creep.pull(target) == ERR_NOT_IN_RANGE) {
+			if(creep.pull(target) == ERR_NOT_IN_RANGE){
 				creep.moveTo(target);
 			} else {
-				target.move(creep);
 				let targetDest = new RoomPosition(target.memory.dest.x, target.memory.dest.y, target.memory.dest.roomName)
+				target.move(creep)
 				if(creep.pos.isNearTo(targetDest)) {
 					creep.move(creep.pos.getDirectionTo(target));
 				} else {
